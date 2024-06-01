444f557a5c64ad50ee30888ca95aaaa9a930aeb0
Uchechukwu Offiah <evaristusoffiah@gmail.com>
2024-06-01 23:22:36 +0100

    Initial commit -m Kafka Consumer

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Confluent.Kafka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var conf = new ConsumerConfig
+        {
+            GroupId = "test-consumer-group",
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+        {
+            c.Subscribe("test-topic");
+
+            try
+            {
+                while (true)
+                {
+                    var cr = c.Consume();
+                    Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
